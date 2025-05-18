ed2bd7fc5deaf2890f65b7dea9e5bb1722c65c09
Venkat <venkatramankannantech@gmail.com>
2025-05-18 17:34:45 +0530

    standardise event route

--- producer/router/router.py
+++ producer/router/router.py
@@ -5,7 +5,7 @@ from producer.service.aws_lambda import invoke_lambda
 
 router = APIRouter()
 
-@router.post("/event", status_code=status.HTTP_202_ACCEPTED)
+@router.post("api/external/v1/event", status_code=status.HTTP_202_ACCEPTED)
 def receive_event(event: Event):
     invoke_lambda(payload=event.model_dump())
     return {"status": "accepted"}
